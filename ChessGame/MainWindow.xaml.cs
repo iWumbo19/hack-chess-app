174b23fcb3410c872fe0b33b4c5c69904ad0a687
Chris Rancourt <rancourtvideo@gmail.com>
2021-12-22 13:41:38 -0500

    Still Struggling.

--- ChessGame/MainWindow.xaml.cs
+++ ChessGame/MainWindow.xaml.cs
@@ -22,7 +22,27 @@ namespace ChessGame
     /// </summary>
     public partial class MainWindow : Window
     {
-        
+
+        Dictionary<ChessPieceType, Bitmap> BlackPieceImage = new Dictionary<ChessPieceType, Bitmap>()
+        {
+            {ChessPieceType.Pawn, Properties.Resources.B_Pawn },
+            {ChessPieceType.Knight, Properties.Resources.B_Knight },
+            {ChessPieceType.Bishop, Properties.Resources.B_Bishop },
+            {ChessPieceType.Queen, Properties.Resources.B_Queen },
+            {ChessPieceType.King, Properties.Resources.B_King },
+            {ChessPieceType.Rook, Properties.Resources.B_Rook }
+        };
+
+        Dictionary<ChessPieceType, Bitmap> WhitePieceImage = new Dictionary<ChessPieceType, Bitmap>()
+        {
+            {ChessPieceType.Pawn, Properties.Resources.W_Pawn },
+            {ChessPieceType.Knight, Properties.Resources.W_Knight },
+            {ChessPieceType.Bishop, Properties.Resources.W_Bishop },
+            {ChessPieceType.Queen, Properties.Resources.W_Queen },
+            {ChessPieceType.King, Properties.Resources.W_King },
+            {ChessPieceType.Rook, Properties.Resources.W_Rook }
+        };
+
         public MainWindow()
         {
             InitializeComponent();
@@ -30,71 +50,15 @@ namespace ChessGame
 
         private void Draw_Button_Click(object sender, RoutedEventArgs e)
         {
-            var brush = new ImageBrush();
-            CurrentBoard.SetupBoard();
-            if (CurrentBoard.Squares[(byte)BoardtoByte.A8].Piece != null)
-                SquareA8 = GetPieceImage(CurrentBoard.Squares[(byte)BoardtoByte.A8].Piece.PieceColor,
-                                            CurrentBoard.Squares[(byte)BoardtoByte.A8].Piece.PieceType);
+            SquareA8.Content = FindResource("B_Rook");
+                
 
         }
 
-        public Bitmap GetPieceImage(ChessPieceColor color, ChessPieceType type)
+        private Bitmap RetrievePieceImage(ChessPieceColor color, ChessPieceType type)
         {
-            ImageConverter ic = new ImageConverter();
-            if (color == ChessPieceColor.White)
-            {
-                switch (type)
-                {
-                    case ChessPieceType.King:                        
-                        System.Drawing.Image img = (System.Drawing.Image)ic.ConvertFrom(Properties.Resources.W_King);
-                        return new Bitmap(img);
-                    case ChessPieceType.Queen:
-                        System.Drawing.Image img2 = (System.Drawing.Image)ic.ConvertFrom(Properties.Resources.W_Queen);
-                        return new Bitmap(img2);
-                    case ChessPieceType.Rook:
-                        System.Drawing.Image img3 = (System.Drawing.Image)ic.ConvertFrom(Properties.Resources.W_Rook);
-                        return new Bitmap(img3);
-                    case ChessPieceType.Bishop:
-                        System.Drawing.Image img4 = (System.Drawing.Image)ic.ConvertFrom(Properties.Resources.W_Bishop);
-                        return new Bitmap(img4);
-                    case ChessPieceType.Knight:
-                        System.Drawing.Image img5 = (System.Drawing.Image)ic.ConvertFrom(Properties.Resources.W_Knight);
-                        return new Bitmap(img5);
-                    case ChessPieceType.Pawn:
-                        System.Drawing.Image img6 = (System.Drawing.Image)ic.ConvertFrom(Properties.Resources.W_Pawn);
-                        return new Bitmap(img6);
-                    default:
-                        System.Drawing.Image img7 = (System.Drawing.Image)ic.ConvertFrom(Properties.Resources.Empty);
-                        return new Bitmap(img7);
-                }
-            }
-            else
-            {
-                switch (type)
-                {
-                    case ChessPieceType.King:
-                        System.Drawing.Image img = (System.Drawing.Image)ic.ConvertFrom(Properties.Resources.B_King);
-                        return new Bitmap(img);
-                    case ChessPieceType.Queen:
-                        System.Drawing.Image img2 = (System.Drawing.Image)ic.ConvertFrom(Properties.Resources.B_Queen);
-                        return new Bitmap(img2);
-                    case ChessPieceType.Rook:
-                        System.Drawing.Image img3 = (System.Drawing.Image)ic.ConvertFrom(Properties.Resources.B_Rook);
-                        return new Bitmap(img3);
-                    case ChessPieceType.Bishop:
-                        System.Drawing.Image img4 = (System.Drawing.Image)ic.ConvertFrom(Properties.Resources.B_Bishop);
-                        return new Bitmap(img4);
-                    case ChessPieceType.Knight:
-                        System.Drawing.Image img5 = (System.Drawing.Image)ic.ConvertFrom(Properties.Resources.B_Knight);
-                        return new Bitmap(img5);
-                    case ChessPieceType.Pawn:
-                        System.Drawing.Image img6 = (System.Drawing.Image)ic.ConvertFrom(Properties.Resources.B_Pawn);
-                        return new Bitmap(img6);
-                    default:
-                        System.Drawing.Image img7 = (System.Drawing.Image)ic.ConvertFrom(Properties.Resources.Empty);
-                        return new Bitmap(img7);
-                }
-            }
+            if (color == ChessPieceColor.White) { return WhitePieceImage[type]; }
+            else { return BlackPieceImage[type]; }
         }
     }
 }
--- ChessGame/Properties/Resources.Designer.cs
+++ ChessGame/Properties/Resources.Designer.cs
@@ -22,7 +22,7 @@ namespace ChessGame.Properties {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources {
+    public class Resources {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace ChessGame.Properties {
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ChessGame.Properties.Resources", typeof(Resources).Assembly);
@@ -51,7 +51,7 @@ namespace ChessGame.Properties {
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        public static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
@@ -61,132 +61,132 @@ namespace ChessGame.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized resource of type System.Byte[].
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static byte[] B_Bishop {
+        public static System.Drawing.Bitmap B_Bishop {
             get {
                 object obj = ResourceManager.GetObject("B_Bishop", resourceCulture);
-                return ((byte[])(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Byte[].
-        /// </summary>
-        internal static byte[] B_King {
-            get {
-                object obj = ResourceManager.GetObject("B_King", resourceCulture);
-                return ((byte[])(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Byte[].
-        /// </summary>
-        internal static byte[] B_Knight {
-            get {
-                object obj = ResourceManager.GetObject("B_Knight", resourceCulture);
-                return ((byte[])(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Byte[].
-        /// </summary>
-        internal static byte[] B_Pawn {
-            get {
-                object obj = ResourceManager.GetObject("B_Pawn", resourceCulture);
-                return ((byte[])(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Byte[].
-        /// </summary>
-        internal static byte[] B_Queen {
-            get {
-                object obj = ResourceManager.GetObject("B_Queen", resourceCulture);
-                return ((byte[])(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Byte[].
-        /// </summary>
-        internal static byte[] B_Rook {
-            get {
-                object obj = ResourceManager.GetObject("B_Rook", resourceCulture);
-                return ((byte[])(obj));
+                return ((System.Drawing.Bitmap)(obj));
             }
         }
         
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap Empty {
+        public static System.Drawing.Bitmap B_King {
             get {
-                object obj = ResourceManager.GetObject("Empty", resourceCulture);
+                object obj = ResourceManager.GetObject("B_King", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
         
         /// <summary>
-        ///   Looks up a localized resource of type System.Byte[].
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static byte[] W_Bishop {
+        public static System.Drawing.Bitmap B_Knight {
+            get {
+                object obj = ResourceManager.GetObject("B_Knight", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        public static System.Drawing.Bitmap B_Pawn {
+            get {
+                object obj = ResourceManager.GetObject("B_Pawn", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        public static System.Drawing.Bitmap B_Queen {
+            get {
+                object obj = ResourceManager.GetObject("B_Queen", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        public static System.Drawing.Bitmap B_Rook {
+            get {
+                object obj = ResourceManager.GetObject("B_Rook", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        public static System.Drawing.Bitmap Chess_Board {
+            get {
+                object obj = ResourceManager.GetObject("Chess_Board", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        public static System.Drawing.Bitmap W_Bishop {
             get {
                 object obj = ResourceManager.GetObject("W_Bishop", resourceCulture);
-                return ((byte[])(obj));
+                return ((System.Drawing.Bitmap)(obj));
             }
         }
         
         /// <summary>
-        ///   Looks up a localized resource of type System.Byte[].
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static byte[] W_King {
+        public static System.Drawing.Bitmap W_King {
             get {
                 object obj = ResourceManager.GetObject("W_King", resourceCulture);
-                return ((byte[])(obj));
+                return ((System.Drawing.Bitmap)(obj));
             }
         }
         
         /// <summary>
-        ///   Looks up a localized resource of type System.Byte[].
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static byte[] W_Knight {
+        public static System.Drawing.Bitmap W_Knight {
             get {
                 object obj = ResourceManager.GetObject("W_Knight", resourceCulture);
-                return ((byte[])(obj));
+                return ((System.Drawing.Bitmap)(obj));
             }
         }
         
         /// <summary>
-        ///   Looks up a localized resource of type System.Byte[].
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static byte[] W_Pawn {
+        public static System.Drawing.Bitmap W_Pawn {
             get {
                 object obj = ResourceManager.GetObject("W_Pawn", resourceCulture);
-                return ((byte[])(obj));
+                return ((System.Drawing.Bitmap)(obj));
             }
         }
         
         /// <summary>
-        ///   Looks up a localized resource of type System.Byte[].
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static byte[] W_Queen {
+        public static System.Drawing.Bitmap W_Queen {
             get {
                 object obj = ResourceManager.GetObject("W_Queen", resourceCulture);
-                return ((byte[])(obj));
+                return ((System.Drawing.Bitmap)(obj));
             }
         }
         
         /// <summary>
-        ///   Looks up a localized resource of type System.Byte[].
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static byte[] W_Rook {
+        public static System.Drawing.Bitmap W_Rook {
             get {
                 object obj = ResourceManager.GetObject("W_Rook", resourceCulture);
-                return ((byte[])(obj));
+                return ((System.Drawing.Bitmap)(obj));
             }
         }
     }
